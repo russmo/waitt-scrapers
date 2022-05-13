56f0a4ad07fbd10d1b8df484314fbce3ea8efc70
Russell Moffitt <russmo@coralhead.com>
2022-05-12 17:33:49 -0700

    label alignment, pilot site, eez set in initiatives/projects google sheet

--- waitt_scrapers/initiatives2geojson.py
+++ waitt_scrapers/initiatives2geojson.py
@@ -11,6 +11,22 @@ sheet_name = 'WI Project Sites'
 # sheet_url_csv = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
 sheet_url_xlsx = f'https://docs.google.com/spreadsheets/d/e/2PACX-1vSIi0bUwNN9xDPGxi7qWZMjxHR-rgyIV8olQWqH7Yc8DLXiGtFgFl4WfoqL_5crumCNQGOpgL_N2h3k/pub?output=xlsx'
 
+def toFloat(f):
+    output = None
+    try:
+        output = float(f)
+    except(ValueError):
+        pass
+    return output
+
+def makeOffset(x,y):
+    output = None
+    try:
+        output = [float(x), float(y)]
+    except(ValueError):
+        pass
+    return output
+
 def initiatives2geojson(outfile, xlsx_url=sheet_url_xlsx):
     df = pd.read_excel(sheet_url_xlsx, sheet_name=sheet_name, na_filter=False)
 
@@ -28,6 +44,11 @@ def initiatives2geojson(outfile, xlsx_url=sheet_url_xlsx):
             "iso3": str(row['EEZ_ISO3']),
             "link": str(row['Link']),
             "image_url": str(row['Hero Image URL']),
+            "pilot": str(row['Pilot']),
+            "label_anchor": str(row['Label Anchor']),
+            "label_x_offset": toFloat(row['Label X Offset']),
+            "label_y_offset": toFloat(row['Label Y Offset']),
+            "label_offset": makeOffset(row['Label X Offset'], row['Label Y Offset']),
         }))
 
 
